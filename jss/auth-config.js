2a21d49c3aa69ddf0089a966b4a51982185d64c9
1-muka <mukagalisulejmenov46@gmail.com>
2026-05-22 01:37:48 +0500

    Add new user to EMERALD_AUTH configuration
    Added a new user 'Abdurashidova_Jasmina' with a unique password.

--- jss/auth-config.js
+++ jss/auth-config.js
@@ -3,6 +3,7 @@ window.EMERALD_AUTH = {
     users: {
         Mukagali: "1234",
         Nodira_Buxarova: "7777",
-        Dilfuza: "1111"
+        Dilfuza: "1111",
+        Abdurashidova_Jasmina: "BPH4tH"
     }
 };
