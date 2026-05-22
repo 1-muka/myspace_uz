d3f457054981d88e607f94d58e276e64bc508c3b
1-muka <mukagalisulejmenov46@gmail.com>
2026-05-22 17:48:03 +0500

    Change password for Abdurashidova_Jasmina
    Updated the password for Abdurashidova_Jasmina.

--- jss/auth-config.js
+++ jss/auth-config.js
@@ -4,6 +4,6 @@ window.EMERALD_AUTH = {
         Mukagali: "1234",
         Nodira_Buxarova: "7777",
         Dilfuza: "1111",
-        Abdurashidova_Jasmina: "BPH4tH",
+        Abdurashidova_Jasmina: "7870882",
     }
 };
